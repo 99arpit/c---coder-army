dd6b0f7400159f789a14379c43df552d94953c8f
Paul Philips <paulphilips@email.com>
2024-12-11 12:41:30 +0530

    first commit

---
+++ First.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Print messages to the console
+    cout << "Try programiz.pro" << endl;
+    cout << "Try programiz.pro" << endl;
+    cout << "Try programiz.pro arpit" << endl;
+
+    return 0; // Indicate successful program termination
+}
